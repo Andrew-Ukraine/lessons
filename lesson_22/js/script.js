dd30108b518d2e012b1679205a83aa9457c82dc1
Andrew <110688299+Andrew-Ukraine@users.noreply.github.com>
2023-03-04 16:20:54 +0200

    22

---
+++ lesson_22/js/script.js
@@ -0,0 +1,41 @@
+'use strict' 
+//Домашка 1 
+console.log("Я вчу JS");
+
+//Домашка 2 
+
+let eyeСolor;
+	// eyeColor = "Red"; 
+let user;
+let userName = "Василь";
+	user = userName;
+	console.log(user);
+
+/* Працює з var
+	if (true) {
+		var size = 15;
+	}
+	console.log(size); 
+	*/
+	/* Не працює з let
+	if (true) {
+		let sizeTwo = 15;
+	}
+	console.log(sizeTwo);*/
+
+/*let name = "Igor";
+let myAge = 60;
+const info = `Ім,я: ${name}, вік: ${myAge}`
+console.log(info);
+console.log(typeof info);
+*/
+
+/*let age = false;
+let myAgeTwo = true;
+if (age && myAgeTwo) {
+	console.log(1);
+}
+else {
+	console.log(2);
+}
+*/
